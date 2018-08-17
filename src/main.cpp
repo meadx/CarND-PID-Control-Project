f9c69000bfc49aace89a8174b9f138c04dee9314
meadx <35892868+meadx@users.noreply.github.com>
2018-08-17 13:37:06 +0200

    Twiddle added

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.1,0.1,0.1);
+  pid.Init(0.0,0.0,0.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -59,6 +59,13 @@ int main()
           * another PID controller to control the speed!
           */
           pid.UpdateError(cte);
+          
+          // Twiddle
+          double tol = 0.2;
+          if (pid.TotalError() > tol) {
+            pid.Twiddle();
+          }
+          
           steer_value = -pid.Kp * pid.p_error - pid.Kd * pid.d_error - pid.Ki * pid.i_error;
           
           // DEBUG
